077874434a0edac6b19044bb995b9dd3d8467ce9
nithin <nithin.linkin@gmail.com>
2014-12-25 05:03:16 +0530

    Need to document

--- script.sh
+++ script.sh
@@ -3,12 +3,48 @@
 #############
 #TODO
 #----
-#Read arguments
+#DONE	Read arguments
 #DONE	Make List of apps
 #DONE	Add to file
 #DONE	Read from file
+#Make list of applications
+#Update usage and help
+#Make readme file
 #Check if already installed
 #############
+
+usage(){
+cat << EOL
+Put error message here.
+EOL
+}
+
+help(){
+cat << EOL
+Put help message here.
+EOL
+}
+
+no_prompt='y'
+
+while getopts ":hp" opt
+do
+	case $opt in
+		h)
+			help
+			exit 1
+			;;
+		p)
+			no_prompt='n'
+			;;
+		?)
+			usage
+			exit 2
+			;;
+	esac
+done
+
+echo $no_prompt
 
 declare -a app_names
 #reading the file into the array
@@ -21,7 +57,6 @@ do
 	fi
 done < "apps_file.txt"
 
-#no_prompt='y'
 
 for app in "${app_names[@]}"
 do
